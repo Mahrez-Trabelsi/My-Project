2d6f15dfbdd5e012b4d34032430e3f87fc4cf64f
Mahrez <mahreztraba@gmail.com>
2023-01-26 20:11:46 +0100

    Added My New Mini_Project

---
+++ Mini_Project/mini-project.js
@@ -0,0 +1,46 @@
+function notification(){
+    //variables declaration
+
+    var weight=document.getElementById("wgh").value;
+    var length=document.getElementById("lh").value;
+
+    //input check conditions
+
+    // Length test
+    if (length == ""){
+    sp1.innerHTML ="Length field is empty! type a value";return false;
+}
+    else{
+        if((length<100)||(length>300)) 
+    { sp1.innerHTML="lenght between 100-300 cm !"; return false;
+    
+        }
+    }
+//Weight test
+    if (weight == ""){
+        sp2.innerHTML="Weight field is empty! type a value";return false;
+    }
+    else{
+        if((weight<30)||(weight>300))
+        { sp2.innerHTML="Weight between 30-300 kg !";return false;
+    }
+    }  
+// Result Printing   
+
+ if ((length == weight)&&( (length !="")&&(weight !=""))){
+            window.location.href ="p_bdy.html"; return false;
+
+    } 
+    else{
+        if (length > weight){
+            window.location.href ="lsw.html"; return false;
+        }
+        else{
+            if ( length < weight ){
+                window.location.href ="gsw.html"; return false;
+            }
+        }
+    }      
+    
+}
+
