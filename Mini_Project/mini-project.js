002149cecc6835dc48ccd28f1b6cea4153d1616d
Mahrez <mahreztraba@gmail.com>
2023-01-27 16:58:49 +0100

    My Portfolio update

--- Mini_Project/mini-project.js
+++ Mini_Project/mini-project.js
@@ -11,20 +11,24 @@ function notification(){
     sp1.innerHTML ="Length field is empty! type a value";return false;
 }
     else{
-        if((length<100)||(length>300)) 
-    { sp1.innerHTML="lenght between 100-300 cm !"; return false;
-    
+        if(isNaN(length)) 
+    { sp1.innerHTML="Length must be a number !"; return false;}
+        else{
+            if((length<100)||(length>300)) 
+            { sp1.innerHTML="lenght between 100-300 cm !"; return false;}
         }
     }
 //Weight test
     if (weight == ""){
         sp2.innerHTML="Weight field is empty! type a value";return false;
     }
-    else{
-        if((weight<30)||(weight>300))
-        { sp2.innerHTML="Weight between 30-300 kg !";return false;
-    }
-    }  
+      else{
+            if(isNaN(weight)) 
+            { sp2.innerHTML="Weight must be a number !"; return false;}
+        else{
+            if((weight<30)||(weight>300))
+            { sp2.innerHTML="Weight between 30-300 kg !";return false;}
+    }  }
 // Result Printing   
 
  if ((length == weight)&&( (length !="")&&(weight !=""))){
